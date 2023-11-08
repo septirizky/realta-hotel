c0c4d095b25357d95a92d86905e72d19b5087273
Mangara Butarbutar <120437609+BtrMangara@users.noreply.github.com>
2023-11-08 11:30:24 +0700

    Update server.js

--- server/src/server.js
+++ server/src/server.js
@@ -1,18 +1,14 @@
-import 'dotenv/config.js'
-import express from 'express';
-import cors from 'cors';
-import router from './routes/index.js';
+import "dotenv/config.js";
+import express from "express";
+import cors from "cors";
+import router from "./routes/index.js";
 
 const app = express();
-const PORT = process.env.PORTS || 4001;
+const PORT = process.env.PORT || 7200;
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.get('/', (req, res) => {
-  res.send('hello world');
-});
 
 app.use(router);
 
