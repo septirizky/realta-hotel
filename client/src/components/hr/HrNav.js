b63d9b82463279dbc61f5907a9ebb10a15182e9f
Sulthan Laksono Ramadhan <1911500641@student.budiluhur.ac.id>
2023-11-17 00:12:12 +0700

    Update sidebar toggle

--- client/src/components/hr/HrNav.js
+++ client/src/components/hr/HrNav.js
@@ -1,31 +1,43 @@
 import {Link, Outlet, useLocation} from "react-router-dom";
 import './css/styles.css'
 import {BsListCheck, BsPeople, BsPerson, BsPersonCircle} from "react-icons/bs";
-import {BiColumns, BiHome} from "react-icons/bi";
 import {MdApartment} from "react-icons/md";
-import {FaAngleDown} from "react-icons/fa";
+import {RxHamburgerMenu} from "react-icons/rx";
+import {useEffect, useState} from "react";
 
 export const HrNav = () => {
     const pathname = useLocation()
+    const [burgerActive, setBurgerActive] = useState(false)
+    const sidebarToggler = (event) => {
+        event.preventDefault();
+        setBurgerActive(!burgerActive)
+        document.body.classList.toggle('sb-sidenav-toggled');
+        localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
+    }
+    useEffect(() => {
+
+    }, [burgerActive]);
     return (
         <body className="sb-nav-fixed">
         <nav className="sb-topnav navbar navbar-expand navbar-dark bg-dark">
             <a className="navbar-brand ps-3" href="#"><MdApartment size='26' className='text-warning'/> Realta Hotel</a>
+            <button className={`btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0 yellow-hover ${burgerActive?"yellow-button":""}`} id="sidebarToggle" href="#!"
+                    onClick={(e) => sidebarToggler(e)}><RxHamburgerMenu size='26'/></button>
             <div className='d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0'>
-                <ul className="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
-                    <li className="nav-item dropdown">
-                        <a className="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button"
-                           data-bs-toggle="dropdown" aria-expanded="false"><BsPersonCircle className='' size='32'/></a>
-                        <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                            <li><a className="dropdown-item" href="#!">Profile</a></li>
-                            <li>
-                                <hr className="dropdown-divider"/>
-                            </li>
-                            <li><a className="dropdown-item" href="#!">Logout</a></li>
-                        </ul>
-                    </li>
-                </ul>
             </div>
+            <ul className="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
+                <li className="nav-item dropdown">
+                    <a className="nav-link dropdown-toggle yellow-hover" id="navbarDropdown" href="#" role="button"
+                       data-bs-toggle="dropdown" aria-expanded="false"><BsPersonCircle className='' size='26'/></a>
+                    <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                        <li><a className="dropdown-item" href="#!">Profile</a></li>
+                        <li>
+                            <hr className="dropdown-divider"/>
+                        </li>
+                        <li><a className="dropdown-item" href="#!">Logout</a></li>
+                    </ul>
+                </li>
+            </ul>
         </nav>
         <div id="layoutSidenav">
             <div id="layoutSidenav_nav">
@@ -34,15 +46,19 @@ export const HrNav = () => {
                         <div className="nav">
                             <div className="sb-sidenav-menu-heading text-warning">Menu</div>
                             <Link to='/hr/department' id='nav_dept'
-                                  className={`nav-link ${pathname.pathname === '/hr/department' ? "active": ""}`}>
+                                  className={`nav-link ${pathname.pathname === '/hr/department' ? "active" : ""}`}>
                                 <div className="sb-nav-link-icon"><BsPeople size='26'/></div>
                                 Department
                             </Link>
-                            <Link to='/hr/employee' className={`nav-link ${pathname.pathname === '/hr/employee' ? "active": ""}`} href="">
+                            <Link to='/hr/employee'
+                                  className={`nav-link ${pathname.pathname === '/hr/employee' ? "active" : ""}`}
+                                  href="">
                                 <div className="sb-nav-link-icon"><BsPerson size='26'/></div>
                                 Employee
                             </Link>
-                            <Link to='/hr/work-order' className={`nav-link ${pathname.pathname === '/hr/work-order' ? "active": ""}`} href="">
+                            <Link to='/hr/work-order'
+                                  className={`nav-link ${pathname.pathname === '/hr/work-order' ? "active" : ""}`}
+                                  href="">
                                 <div className="sb-nav-link-icon"><BsListCheck size='26'/></div>
                                 Work Order
                             </Link>
