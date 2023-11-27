7f8fec53df547074dd1ea084172192c10d54543a
rahmatrafli1 <rahmatrafli1@gmail.com>
2023-11-27 10:35:47 +0700

    membuat fitur detail price items

--- client/src/components/master/ListPriceItems/GetAllPriceItems.jsx
+++ client/src/components/master/ListPriceItems/GetAllPriceItems.jsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import SearchItems from "./Search";
 import { AiOutlinePlus } from "react-icons/ai";
 import { FaPencilAlt, FaTimes } from "react-icons/fa";
 import { MdKeyboardDoubleArrowRight } from "react-icons/md";
+import DetailPriceItems from "./modal/DetailPriceItems";
 
 const GetAllPriceItems = (props) => {
   const formatRupiah = (number) => {
@@ -22,6 +23,42 @@ const GetAllPriceItems = (props) => {
     setKueri2,
     searchData,
   } = props;
+
+  const [showModalDetailItemPrice, setShowModalDetailItemPrice] =
+    useState(false);
+
+  const [itemPriceId, setItemPriceId] = useState();
+
+  const [itemPriceName, setItemPriceName] = useState("");
+
+  const [itemPricePrit, setItemPricePrit] = useState("");
+
+  const [itemPriceDescription, setItemPriceDescription] = useState("");
+
+  const [itemPriceType, setItemPriceType] = useState("");
+
+  const [itemPriceIcon, setItemPriceIcon] = useState("");
+
+  const getShowDetailItemPrice = (
+    itemPriceId,
+    itemPriceName,
+    itemPricePrit,
+    itemPriceDescription,
+    itemPriceType,
+    itemPriceIcon
+  ) => {
+    setItemPriceId(itemPriceId);
+    setItemPriceName(itemPriceName);
+    setItemPricePrit(itemPricePrit);
+    setItemPriceDescription(itemPriceDescription);
+    setItemPriceType(itemPriceType);
+    setItemPriceIcon(itemPriceIcon);
+    setShowModalDetailItemPrice(true);
+  };
+
+  const closeDetailItemPrice = () => {
+    setShowModalDetailItemPrice(false);
+  };
 
   return (
     <>
@@ -65,6 +102,7 @@ const GetAllPriceItems = (props) => {
                       <img
                         src={iprice.prit_icon_url}
                         alt={iprice.prit_icon}
+                        className="rounded-circle"
                         width={30}
                         height={30}
                       />
@@ -75,7 +113,22 @@ const GetAllPriceItems = (props) => {
                       {formatRupiah(iprice.prit_price)}
                     </td>
                     <td className="text-end">
-                      <MdKeyboardDoubleArrowRight />
+                      <button
+                        type="button"
+                        className="button-detail-transparan"
+                        onClick={() =>
+                          getShowDetailItemPrice(
+                            iprice.prit_id,
+                            iprice.prit_name,
+                            iprice.prit_price,
+                            iprice.prit_description,
+                            iprice.prit_type,
+                            iprice.prit_icon_url
+                          )
+                        }
+                      >
+                        <MdKeyboardDoubleArrowRight />
+                      </button>
                     </td>
                     <td>{iprice.prit_type}</td>
                     <td className="align-border-right">
@@ -106,6 +159,17 @@ const GetAllPriceItems = (props) => {
           </tbody>
         )}
       </table>
+
+      <DetailPriceItems
+        showModalItemPrice={showModalDetailItemPrice}
+        handleCloseDetailItemPrice={closeDetailItemPrice}
+        itemPriceId={itemPriceId}
+        itemPriceName={itemPriceName}
+        itemPricePrit={itemPricePrit}
+        itemPriceDescription={itemPriceDescription}
+        itemPriceType={itemPriceType}
+        itemPriceIcon={itemPriceIcon}
+      />
     </>
   );
 };
