54ed4595c602d931dc95837382482222501c4c07
Sulthan Laksono Ramadhan <1911500641@student.budiluhur.ac.id>
2023-11-30 00:05:33 +0700

    Minor Fix

--- client/src/index.js
+++ client/src/index.js
@@ -8,14 +8,8 @@ import thunk from "redux-thunk";
 import { Provider } from "react-redux";
 import reducers from "./reducers";
 
-const store = createStore(
-  reducers,
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const store = createStore(reducers, compose(applyMiddleware(thunk)))
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
