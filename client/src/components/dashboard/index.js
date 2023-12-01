89e154cdc0b8cc9a24d0d4fcaebf2475d143d1e7
rahmatrafli1 <rahmatrafli1@gmail.com>
2023-12-01 20:04:34 +0700

    menambahkan breadcrumb pada halaman dashboard

--- client/src/components/dashboard/index.js
+++ client/src/components/dashboard/index.js
@@ -1,7 +1,18 @@
+import { FaHome } from "react-icons/fa";
+
 export const Dashboard = () => {
-    return (
-        <div>
-            <h1>Dashboard</h1>
-        </div>
-    )
-}
+  return (
+    <>
+      <div className="border border-black container-fluid pt-3 mb-3">
+        <nav className="bread-separator" aria-label="breadcrumb">
+          <ol className="breadcrumb">
+            <li className="breadcrumb-item active">
+              <FaHome className="me-2" /> Home
+            </li>
+          </ol>
+        </nav>
+      </div>
+      <h1 className="text-center">Dashboard</h1>
+    </>
+  );
+};
